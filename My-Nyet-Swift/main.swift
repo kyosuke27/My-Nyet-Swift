c10b6ec8fad169e0f8b3369ae130b22712af65f3
kyosuke <kn1359028527@gmail.com>
2025-12-14 22:03:19 +0900

    Add Monster Data

--- My-Nyet-Swift/main.swift
+++ My-Nyet-Swift/main.swift
@@ -1,23 +1,65 @@
 import Foundation
 
-print("Hello, World!")
-// Input Command Line
-let inputLine = readLine()!
+main()
 
-print("inputLine : \(inputLine)")
-print(showMonster())
+func main(){
+    
+    // Input Command Line
+    let inputLine = readLine()!
+    
+    print("inputLine : \(inputLine)")
+    printDash()
+    //    print(green + showMonster(name: inputLine) + reset)
+    print(retMonsterName(name: inputLine))
+    terminalPrint(text: showMonster(monster:MonsterType.Slime), color: TerminalColor.red)
+    printDash()
+    
+}
 
-func showMonster()->String{
-  return #"""
-            ^
-          _/ \_
-        _/     \_
-      _/         \_
-     /   o     o   \
+func printDash(){
+    print("=====================================")
+}
+
+// for terminal use
+func terminalPrint(text:String,color:TerminalColor){
+    print(color.rawValue+text+TerminalColor.reset.rawValue)
+}
+
+func colorString(text:String,color:TerminalColor)->String{
+    return color.rawValue+text+TerminalColor.reset.rawValue
+}
+
+// MonsterName
+func retMonsterName(name:String)->String{
+    return "Monster Name \(colorString(text: name, color: TerminalColor.green))"
+}
+
+enum MonsterType{
+    case Slime
+}
+
+func showMonster(monster:MonsterType)->String{
+    switch monster {
+    case .Slime:
+        return """
+           _^_
+        _/     \\_
+      _/         \\_
+     /   o     o   \\
     |      ___      |
-    |     \___/     |
-     \             /
-      \___________/
+    |     \\___/     |
+     \\             /
+      \\___________/
+"""
+    }
+}
 
-"""#
+// enum form text color
+
+enum TerminalColor:String{
+    case reset = "\u{001B}[0m"
+    
+    case blue = "\u{001B}[34m"
+    case red = "\u{001B}[31m"
+    case green = "\u{001B}[32m"
 }
