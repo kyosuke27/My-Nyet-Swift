fe9195b1844ce3d41d00c141b4642900c9fdcd8f
kyosuke <kn1359028527@gmail.com>
2025-12-20 10:29:09 +0900

    一文字インプットファイルを追加

---
+++ Sources/ReadChar.swift
@@ -0,0 +1,24 @@
+import Foundation
+func readChar() -> Character? {
+    var oldTerm = termios()
+    var newTerm = termios()
+
+    tcgetattr(STDIN_FILENO, &oldTerm)
+    newTerm = oldTerm
+
+    // カノニカルモード & エコーを無効化
+    newTerm.c_lflag &= ~(UInt(ICANON | ECHO))
+    tcsetattr(STDIN_FILENO, TCSANOW, &newTerm)
+
+    defer {
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldTerm)
+    }
+
+    var buffer: UInt8 = 0
+    let count = read(STDIN_FILENO, &buffer, 1)
+
+    if count == 1 {
+        return Character(UnicodeScalar(buffer))
+    }
+    return nil
+}
